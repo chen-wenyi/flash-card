a2d88a37c6663d865e4536f918bc9469538d659e
wenyichen <chwwyi@163.com>
2024-12-23 09:33:28 +1300

    feat: add drawer

---
+++ src/app/card/[id]/layout.tsx
@@ -0,0 +1,7 @@
+export default function CardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <section className='m-4'>{children}</section>
+}
--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -1,8 +1,20 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import useStore from '@/lib/useStore';
 import { useFcStore } from '@/useFcStore';
 import { motion } from 'motion/react';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from './ui/drawer';
 
 export default function CardDetail({ id }: { id: number }) {
   const state = useStore(useFcStore, (state) => state);
@@ -17,8 +29,39 @@ export default function CardDetail({ id }: { id: number }) {
         ease: [0, 0.71, 0.2, 1.01],
       }}
     >
-      <div>{detail?.title}</div>
-      <div>{detail?.desc}</div>
+      <div className='font-bold'>{detail?.title}</div>
+      <Separator className='my-2' />
+      <div className='max-h-[45vh] overflow-auto text-sm'>{detail?.desc}</div>
+      <Separator className='my-2' />
+      <div className='flex flex-col gap-4'>
+        <div className='font-bold'>Question 1</div>
+        <div className='text-sm'>当你洗冷水澡时，立刻促进血液循环，会让你瞬间充满能量。</div>
+        <Drawer>
+          <DrawerTrigger>
+            <Button variant='default' asChild>
+              <div className='w-full'>Show Answer</div>
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle></DrawerTitle>
+              <DrawerDescription className='text-left'>
+                when you take a cold shower, it instantly boost blood circulation, making you energised right away.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <Button asChild>
+                <div>Ok</div>
+              </Button>
+              <DrawerClose>
+                <Button asChild variant='outline' className='w-full'>
+                  <div>Again</div>
+                </Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </motion.div>
   );
 }
