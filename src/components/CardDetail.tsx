a368c46f9cb4bfe98d5f31b6bf52e6c4367219f9
wenyichen <chwwyi@163.com>
2024-12-23 08:59:58 +1300

    feat: add animation

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -2,14 +2,23 @@
 
 import useStore from '@/lib/useStore';
 import { useFcStore } from '@/useFcStore';
+import { motion } from 'motion/react';
 
 export default function CardDetail({ id }: { id: number }) {
   const state = useStore(useFcStore, (state) => state);
   const detail = state?.cards.find((card) => card.id === id);
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
       <div>{detail?.title}</div>
       <div>{detail?.desc}</div>
-    </div>
+    </motion.div>
   );
 }
