43fe320becb5d6b98bd73a7daad6dc9ef8d0ed2a
wenyichen <chwwyi@163.com>
2024-12-25 11:30:50 +1300

    feat: add redis

--- src/useFcStore.ts
+++ src/useFcStore.ts
@@ -1,46 +1,35 @@
+import { Redis } from '@upstash/redis';
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { createJSONStorage, persist, StateStorage } from 'zustand/middleware';
 import { Card } from './typing';
+
+const redis = new Redis({
+  url: 'https://smart-kite-52899.upstash.io',
+  token: 'Ac6jAAIjcDEyZTY2NDdjYjQyYzc0NjA2YWFjYjhiNzcxNjhiYjE4Y3AxMA',
+});
 
 export interface AppState {
   cards: Card[];
   updateReviseTime: (id: number, reviseTime: number) => void;
 }
-const initialCards: Card[] = [
-  {
-    id: 1,
-    title: '洗冷水澡',
-    desc: '当你洗冷水澡时，立刻促进血液循环，会让你瞬间充满能量。如果你反复这样做，它就会逐渐变成一种习惯。这会让你一直做下去，这也是建立自信的来源，你也开始真正去了了解自己。说到自信，我们往往试图从外界寻找自信，比如别人对我们的看法、我们给他人的印象。但最终，自信是来自内心的。给自己设定一些小目标，努力完成它们，享受挑战的过程，并时刻保持自律，这才是建立自信的关键。',
-    answer:
-      'when you take a cold shower, it instantly boost blood circulation, making you energised right away.',
-    hint: '',
-    reviseTime: -1,
+
+const initialCards: Card[] = [];
+
+// Custom storage object
+const storage: StateStorage = {
+  getItem: async (name: string): Promise<string | null> => {
+    console.log(name, 'has been retrieved');
+    return (JSON.stringify(await redis.json.get(name))) || null;
   },
-  {
-    id: 2,
-    title: '2',
-    desc: '2',
-    answer: '2',
-    hint: '',
-    reviseTime: -1,
+  setItem: async (name: string, value: string): Promise<void> => {
+    console.log(name, 'with value', value, 'has been saved');
+    await redis.json.set(name, '$', value);
   },
-  {
-    id: 3,
-    title: '3',
-    desc: '3',
-    answer: '3',
-    hint: '',
-    reviseTime: -1,
+  removeItem: async (name: string): Promise<void> => {
+    console.log(name, 'has been deleted');
+    // await del(name)
   },
-  {
-    id: 4,
-    title: '4',
-    desc: '4',
-    answer: '4',
-    hint: '',
-    reviseTime: -1,
-  },
-];
+};
 
 export const useFcStore = create<AppState>()(
   persist(
@@ -58,8 +47,8 @@ export const useFcStore = create<AppState>()(
         })),
     }),
     {
-      name: 'flash-card-storage',
-      storage: createJSONStorage(() => sessionStorage),
+      name: 'flashcard',
+      storage: createJSONStorage(() => storage),
     }
   )
 );
